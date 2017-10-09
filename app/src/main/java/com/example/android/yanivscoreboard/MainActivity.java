dbf23d2d9a7770a4b4cc5df3e364dfd7903c7804
Shai Gotlieb <shaigotlieb@gmail.com>
2017-10-09 16:21:29 +0300

    spelling correction

--- app/src/main/java/com/example/android/yanivscoreboard/MainActivity.java
+++ app/src/main/java/com/example/android/yanivscoreboard/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
     int val3;
     int val4;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
